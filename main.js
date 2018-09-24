14b069c15ebde65cecd2dafc7883224b5d545e4e
Monica Chang <monicac1@andrew.cmu.edu>
2018-09-24 13:52:20 -0400

    git commit -m "added an editing functionality to list items"

--- main.js
+++ main.js
@@ -24,3 +24,27 @@ function completeListItem(item) {
         // in css, this would be: "text-decoration: none"
     }
 }
+
+function editListItem(item) {
+    var listItem = item;
+    var newInputBox = document.createElement("input");
+    listItem.appendChild(newInputBox);
+    // listItem.getElementsByClassName("listText").innerHMTL = newInputBox.value;
+    var submitButton = document.createElement("button");
+    submitButton.innerHTML = "Done";
+    submitButton.className = "doneButton";
+    listItem.appendChild(submitButton);
+    // call the function when you click the done button
+    submitButton.addEventListener("click", function() {doneButtonClicked(submitButton)});
+}
+
+function doneButtonClicked(submitButton) {
+    inputBox = submitButton.previousSibling;
+    var listItem = submitButton.parentNode;
+    // make sure you are getting a single element of the array
+    var listItemName = listItem.getElementsByTagName("span")[0];
+    listItemName.innerHTML = inputBox.value;
+    // hide the input box after changes are made
+    inputBox.style.display = "none";
+    submitButton.style.display = "none";
+}
